bc92438c087c8fe458463a1e59b4f85d88d909b8
EvgenPol <evgenpol97@gmail.com>
2021-05-24 15:26:55 +0300

    Revert "update"
    This reverts commit 10832b6b54e7add052a33b17346e35a3b1266d91.

---
+++ D&D Player's Character/D&D Player's Character/Model/Item/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  D&D Player's Character
+//
+//  Created by Евгений Полюбин on 18.05.2021.
+//
+
+import Foundation
+
+class Item {
+    var cost:Int!
+    var weight:Int!
+    var size:Size!
+}
