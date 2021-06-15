f5e8f845b7a8eb27d3712ca72581d6da25868f74
EvgenPol <evgenpol97@gmail.com>
2021-06-15 19:17:23 +0300

    Update 15.06

--- D&D Player's Character/D&D Player's Character/Model/Item/Equipment.swift
+++ D&D Player's Character/D&D Player's Character/Model/Item/Equipment.swift
@@ -10,16 +10,7 @@ import Foundation
 enum Equipment {
     //Item
     enum Item {
-        case abacus
-        case acidVial
-        case alchemistsFireFlask
-        enum Ammunition {
-            case arrows
-            case blowgunNeedles
-            case crossbowBolts
-            case slingBullets
-        }
-        case antitoxinVial
+       
         enum ArcaneFocus {
             case crystal
             case orb
@@ -27,97 +18,20 @@ enum Equipment {
             case staff
             case wand
         }
-        case backpack
-        case ballBearings
-        case barrel
-        case basket
-        case bedroll
-        case bell
-        case blanket
-        case blockAndTackle
-        case book
-        case bootle
-        case glass
-        case bucket
-        case caltrops
-        case candle
-        case caseCrossbowBolt
-        case caseMapOrScroll
-        case chain
-        case chalk
-        case chest
-        case climbersKit
-        case clothesCommon
-        case clothesCostume
-        case clothesFine
-        case clothesTraveler
-        case componentPouch
-        case  crowbar
+       
         enum DruidicFocus {
             case springOfMistletoe
             case totem
             case woodenStaff
             case yewWand
         }
-        case fishingTackle
-        case flaskOfTankard
-        case grapplingHook
-        case hammer
-        case hammerSledge
-        case healersKit
+      
         enum HolySymbol {
             case amulet
             case emblem
             case reliquary
         }
-        case holyWaterFlask
-        case hourglass
-        case huntingTrap
-        case inkBotle
-        case inkPen
-        case jug
-        case ladder
-        case lamp
-        case lanternBullseye
-        case lanternHooded
-        case lock
-        case magnifyingGlass
-        case manacles
-        case messKit
-        case mirrorSteel
-        case oilFlask
-        case paper
-        case parchment
-        case perfumeVial
-        case pickMiners
-        case piton
-        case poisonBasicVial
-        case pole
-        case potIron
-        case potionOfHealing
-        case pouch
-        case quiver
-        case ramPortable
-        case rations
-        case robes
-        case ropeHempen
-        case ropeSilk
-        case sack
-        case scaleMerchants
-        case sealingWax
-        case shovel
-        case signalWhistle
-        case signetRing
-        case soap
-        case spellbook
-        case spikesIron
-        case spyglass
-        case tent
-        case tinderbox
-        case torch
-        case vial
-        case waterskin
-        case whetstone
+        
     }
     enum EquipmentPacks {
         case burglarsPack
--- D&D Player's Character/D&D Player's Character/Model/Item/Items.swift
+++ D&D Player's Character/D&D Player's Character/Model/Item/Items.swift
@@ -6,14 +6,15 @@
 //
 
 import Foundation
+//MARK: Аммуниция
 class Ammunition: Item {
-    var ammunition:Ammo
+    let ammunition:Ammo
     enum Ammo {
         case arrow
         case blowgunNeedles
         case crossbowBolt
         case slingBullets
-        var rawValue: (Double, Double) {
+        var rawValue: (cost:Double, weight:Double) {
                switch self {
                case .arrow: return (5, 0.05)
                case .blowgunNeedles: return (2, 0.02)
@@ -23,6 +24,52 @@ class Ammunition: Item {
         }
     }
     init(ammunition: Ammo) {
+        var name = ""
+        switch ammunition {
+        case .arrow: name = "Стрела"
+        case .blowgunNeedles: name = "Дротик для духового оружия"
+        case .crossbowBolt: name = "Болт"
+        case .slingBullets: name = "Снаряд для пращи"
+        }
+        super.init(name: name, cost: ammunition.rawValue.cost, weight: ammunition.rawValue.weight, description: "")
         self.ammunition = ammunition
     }
+}
+class OtherItems: Item {
+    let item: Items
+    enum Items {
+        case abacus, acidVial, alchemistsFireFlask, antitoxinVial, backpack, ballBearings, barrel, basket, bedroll, bell ,blanket, blockAndTackle, book, bootle, glass, bucket, caltrops, candle, caseCrossbowBolt, caseMapOrScroll, chain, chalk, chest, climbersKit, clothesCommon, clothesCostume, clothesFine, clothesTraveler, componentPouch, crowbar, fishingTackle, flaskOfTankard, grapplingHook,hammer, hammerSledge, healersKit, holyWaterFlask, hourglass, huntingTrap, inkBotle, inkPen, jug, ladder, lamp,lanternBullseye,lanternHooded,lock,magnifyingGlass, manacles, messKit, mirrorSteel, oilFlask, paper, parchment, perfumeVial, pickMiners, piton, poisonBasicVial, pole, potIron, potionOfHealing, pouch, quiver, ramPortable,rations, robes,ropeHempen, ropeSilk, sack, scaleMerchants, sealingWax, shovel, signalWhistle, signetRing,soap,spellbook,         spikesIron, spyglass, tent, tinderbox, torch, vial, waterskin, whetstone
+    }
+    private init(item:Items, name:String, cost:Double,weight:Double, description:String){
+        super.init(name: name, cost: cost, weight: weight, description: description)
+        self.item = item
+    }
+}
+//MARK: Стартовые паки
+class Pack: Item {
+    let pack:Packs
+    enum Packs {
+        case burglar
+        case diplomat
+        case dungeoneer
+        case entertainer
+        case explorer
+        case priest
+        case scholar
+    }
+    init(pack:Packs) {
+        <#statements#>
+    }
+}
+extension OtherItems {
+    var rawValue: (name:String, cost:Double, weight:Double) {
+        switch item {
+        case .abacus: return ("Счёты", 2_00, 2)
+        case .acidVial: return ("")
+//        case .antitoxinVial: return ("Противоядие",50_00, 0.1)
+        case .
+            
+        
+    }
+    
 }
